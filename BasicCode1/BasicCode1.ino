53d9c7019213e8501382973a804412c84075b9e3
Bluejay701 <susanxiang701@gmail.com>
2025-05-19 09:30:18 -0700

    test

--- BasicCode1/BasicCode1.ino
+++ BasicCode1/BasicCode1.ino
@@ -25,22 +25,25 @@ void doCalibration();
 void getPID_error();
 void getData();
 
-int minimum[8] = {577,484,484,415,484,484,461,812};
+int minimum[8] = {0};//{577,484,484,415,484,484,461,812};
 int maximum[8] = {1923,1328,1375,849,1375,923,1493};
 int weights[8] = {-8, -4, -2, -1, 1, 2, 4, 8};
-const double MAXSPEED=255;
+const double MAXSPEED=30;
 double error=0;
 double last_error=0;
 double deltaE = 0;
 double totalError = 0;
 double pid_error = 0;
 
-double spd=70;
-uint16_t = previousTime = 0;
-uint16_t = currentTime = 0;
+double spd=15;
+uint16_t previousTime = 0;
+uint16_t currentTime = 0;
 const int interval = 6; //ms
 bool PID_ON=true;
-double kp, kd, ki;
+//double kp, kd, ki;
+double kp = -0.004;
+double kd = -0.1;
+double ki = 0;
 
 ///////////////////////////////////
 void setup() {
@@ -61,7 +64,7 @@ void setup() {
 
   pinMode(LED_RF, OUTPUT);
   
-//  ECE3_Init();
+  ECE3_Init();
 
 // set the data rate in bits/second for serial data transmission
   Serial.begin(9600); 
@@ -69,30 +72,30 @@ void setup() {
   
 }
 
-void doCalibration(){
-  for(int n=0; n<8; n++){
-    all_sets[8][8] = ECE3_read_IR(sensorValues);
-  }
-  for(int j=0; j<8; j++){ //iterate through each row
-    int maximumVal=all_set[j][0];
-    int minimumVal=all_set[j][0];
-    for (int i=0; i<8; i++){  //iterate through each column
-      if(all_sets[j][k]>maximumVal){
-        maximum[j] = all_sets[j][k];
-      }
-      if(all_sets[j][k]<minimumVal){
-        minimum[j] = all_sets[j][k];
-      }
-   
-    }
-  }
-  
-}
+//void doCalibration(){
+//  for(int n=0; n<8; n++){
+//    all_sets[8][8] = ECE3_read_IR(sensorValues);
+//  }
+//  for(int j=0; j<8; j++){ //iterate through each row
+//    int maximumVal=all_set[j][0];
+//    int minimumVal=all_set[j][0];
+//    for (int i=0; i<8; i++){  //iterate through each column
+//      if(all_sets[j][k]>maximumVal){
+//        maximum[j] = all_sets[j][k];
+//      }
+//      if(all_sets[j][k]<minimumVal){
+//        minimum[j] = all_sets[j][k];
+//      }
+//   
+//    }
+//  }
+//  
+//}
 
 void getPID_error(){
-  deltaError = error-last_error;
-  totalError += changeError;
-  pid_error = kp*error + kd*deltaError + ki*totalError;
+  deltaE = error-last_error;
+  totalError += error;
+  pid_error = kp*error + kd*deltaE + ki*totalError;
   if(spd+pid_error>MAXSPEED){
     pid_error = MAXSPEED-spd;
   }else if ((spd-pid_error)<-MAXSPEED){
@@ -102,29 +105,38 @@ void getPID_error(){
 }
 
 void getError(){
+  error = 0;
   for(int k=0; k<8; k++){
-    sensor_values[k] -= minimum[k];
-    sensor_values[k]*= 1000/maximum[k];
+    if (sensor_values[k] < minimum[k]) {
+      sensor_values[k] = 0;
+    } else {
+      sensor_values[k] -= minimum[k];
+    }
+    
+    //sensor_values[k]*= 1000/maximum[k];
     error += sensor_values[k]*weights[k];
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  currentTime = millis();
+//  currentTime = millis();
   if(PID_ON){
-    if((currentTime-previousTime)>interval){
-      doCalibration();
-      previousTime=currentTime;
-    }
-    //ECE3_read_IR(sensorValues);
+//    if((currentTime-previousTime)>interval){
+//      doCalibration();
+//      previousTime=currentTime;
+//    }
+    ECE3_read_IR(sensorValues);
 
     for(int i=0; i<8; i++){
-      sensor_values[i] = static_cast<int>(sensorValues[i]);
+      sensor_values[i] = sensorValues[i];
+//      Serial.print(sensor_values[i]);
+//      Serial.print('\t');
     }
+//    Serial.println();
   getError();
   getPID_error();
-  // Serial.println(error);
+  Serial.println(error);
   //+error: center of car too much to the left (+pid_error to left)
   //-error: center of car too much to the right (+pid_error to right)
   analogWrite(left_pwm_pin,spd+pid_error);
@@ -141,7 +153,7 @@ void loop() {
 
 // 
   
-  ECE3_read_IR(sensorValues);
+//  ECE3_read_IR(sensorValues);
 
   // digitalWrite(LED_RF, HIGH);
   // delay(250);
